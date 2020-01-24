4b7eeb6a92d685218908feec92db5009bf5e0ee5
Bilge Adam <Bilge Adam@CBYG-DAN-0TLRW3.bilgeadam.com>
2020-01-24 14:20:16 +0300

    init

---
+++ src/Acme.BookStore.Web/Pages/Books/index.js
@@ -0,0 +1,26 @@
+$(function () {
+
+    var createModal = new abp.ModalManager(abp.appPath + 'Books/CreateModal');
+
+    createModal.onResult(function () {
+        dataTable.ajax.reload();
+    });
+
+    $('#NewBookButton').click(function (e) {
+        e.preventDefault();
+        createModal.open();
+    });
+
+    var dataTable = $('#BooksTable').DataTable(abp.libs.datatables.normalizeConfiguration({
+        ajax: abp.libs.datatables.createAjax(acme.bookStore.book.getList),
+        columnDefs: [
+            { data: "name" },
+            { data: "type" },
+            { data: "publishDate" },
+            { data: "price" },
+            { data: "creationTime" }
+        ]
+    }));
+});
+
+
